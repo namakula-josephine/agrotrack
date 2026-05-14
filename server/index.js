f95e420dcc39415926d952507ee9e389f9a50cff
Namakula Josephine <jnamakula324@gmail.com>
2026-05-14 22:39:34 +0300

    update CORS

--- server/index.js
+++ server/index.js
@@ -5,7 +5,12 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: 'https://agrotrack-ten.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
+
 app.use(express.json())
 
 const farmersRoute = require('./routes/farmers')
